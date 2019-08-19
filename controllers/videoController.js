fe9498a3fcb52a79be312bc0edd5c83bea031502
username <user@email.mail>
2019-08-19 22:40:55 +0900

    0819

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,17 +1,23 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) => {
-    // const query = req.query.term; ES6 이전방식
-
-   //컨트롤러도 query에 접근하려면, method가 get이어야 한다
-    const { query: { term: searchingBy } } = req;
-     //제약선언 { req객체 안에 query : {queryName:변수명지정}} = req 객체
-     
-    //res.render("search", { pageTitle: "Search", searchingBy: searchingBy }); 
-    res.render("search", { pageTitle: "Search", searchingBy}); //위와 같다
-    
+import { videos } from "../db";
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
 };
-export const videos = (req, res) => res.render("videos", { pageTitle: "Videos" });
-export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
-export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
-export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
-export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
+
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
+
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageTitle: "Video Detail" });
+
+export const editVideo = (req, res) =>
+  res.render("editVideo", { pageTitle: "Edit Video" });
+
+export const deleteVideo = (req, res) =>
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,8 +1,13 @@
 //url요청을 쪼개서 관리
 import express from "express";
+import routes from "../routes";
+import { upload,videoDetail ,editVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
+videoRouter.get(routes.upload,upload);
+videoRouter.get(routes.videoDetail,videoDetail);
+videoRouter.get(routes.editVideo,editVideo);
 
 //코드 전체를 export함
 export default videoRouter;
